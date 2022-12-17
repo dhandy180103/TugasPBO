f10217f95b064fb9111c8116127bcb20ada9d4d4
dhandy180103 <101711694+dhandy180103@users.noreply.github.com>
2022-12-17 22:21:28 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,57 +1,24 @@
-import java.util.Scanner;
 
 public class Customer {
-    private String nik;
-    private String namaCustomer;
-    private String noHp;
-    private String alamat;
+    private Rental rental;
 
-    public char[] getNik() {
-        return nik;
+    public Customer(Rental rental){
+        this.rental = rental;
     }
 
-    public void setNik(char[] nik) {
-        this.nik = nik;
+    void isiForm(){
+        this.rental.Form();
     }
 
-    public String getNamaCustomer() {
-        return namaCustomer;
+    void memilih(){
+        this.rental.pilih();
+    }
+    void melakukanTransaksi(){
+        this.rental.transaksi();
     }
 
-    public void setNamaCustomer(String namaCustomer) {
-        this.namaCustomer = namaCustomer;
+    void pembayaran(){
+        this.rental.bayar();
     }
 
-    public String getNoHp() {
-        return noHp;
-    }
-
-    public void setNoHp(String noHp) {
-        this.noHp = noHp;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public void isiForm(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("FORM DATA DIRI USER");
-        System.out.print("Masukan Nama Anda : ");
-        String nama = in.nextLine();
-        System.out.print("Masukan NIK anda : ");
-        String nik = in.nextLine();
-        System.out.print("Masukan Nomer Hp Anda : ");
-        String noHp = in.nextLine();
-        System.out.print("Masukan Alamat Anda : ");
-        String alamat = in.nextLine();
-    }
-
-    public void bayar(){
-
-    }
 }
