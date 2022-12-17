35f2ae569546bae86588dc6a9702d6427327e7a9
dhandy180103 <101711694+dhandy180103@users.noreply.github.com>
2022-12-17 22:25:30 +0700

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -10,10 +10,11 @@ public class Motor extends Kendaraan{
         System.out.println("======================");
         System.out.println("Pilih Jenis Motor");
         System.out.println("======================");
-        System.out.println("1.Honda vario");
-        System.out.println("2.Honda beat");
-        System.out.println("3.Yamaha Nmax");
-        System.out.println("4.Yamaha Mio");
+        System.out.println("1.Honda Vario");
+        System.out.println("2.Honda Beat");
+        System.out.println("3.Yamaha Aerox 155CC");
+        System.out.println("4.Yamaha N-MAXX New");
+        System.out.println("5.Vespa Matic");
         System.out.print("Masukkan pilihan : ");
         pilihKendaraan();
     }
@@ -28,10 +29,27 @@ public class Motor extends Kendaraan{
             case 1 :
                 System.out.println("Anda Memilih Honda Vario");
                 jenis = "Honda Vario";
+                Kendaraan.harga = 150000;
                 break;
             case 2 :
                 System.out.println("Anda Memilih Honda Beat");
                 jenis = "Honda Beat";
+                Kendaraan.harga = 130000;
+                break;
+            case 3 :
+                System.out.println("Anda Memilih Aerox 155CC");
+                jenis = "Aerox 155CC";
+                Kendaraan.harga = 140000;
+                break;
+            case 4 :
+                System.out.println("Anda Memilih N-MAX New");
+                jenis = "N-Maxx New";
+                Kendaraan.harga = 170000;
+                break;
+            case 5 :
+                System.out.println("Anda Memilih Vespa Matic");
+                jenis = "Vespa Matic";
+                Kendaraan.harga = 200000;
                 break;
         }
     }
